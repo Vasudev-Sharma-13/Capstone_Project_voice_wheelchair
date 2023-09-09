c614120298451f93d7d4bfb6275434aaf255f2c0
Vasudev-Sharma-13 <117604604+Vasudev-Sharma-13@users.noreply.github.com>
2023-09-09 11:04:13 -0400

    Added support for ultrasonic sensor
    Added support for ultrasonic sensor

--- Wheelchair_arduino/Wheelchair_arduino.ino
+++ Wheelchair_arduino/Wheelchair_arduino.ino
@@ -2,11 +2,22 @@
 # include <NewPing.h>
 
 
-SoftwareSerial bluetooth(2, 3); //for Bluetooth module
+SoftwareSerial bluetooth(4,5); //for Bluetooth module RX FOLLOWD BY TX
+
+NewPing sonar1(10,11,100);
+NewPing sonar2(12,13,100);
 
 void setup() {
 Serial.begin(9600);
 bluetooth.begin(9600);  
+
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+digitalWrite(2, HIGH);
+digitalWrite(3, HIGH);
+
+
+
 pinMode(6, OUTPUT); //LEFT motor
 pinMode(7, OUTPUT); //LEFT motor
 
@@ -17,6 +28,7 @@ pinMode(A0, OUTPUT);  //Right motor
 pinMode(A1, OUTPUT);  //Left motor      
 analogWrite(A0,255);  //Right motor speed PWM
 analogWrite(A1,255);  //Left motor speed PWM
+pinMode(A2,OUTPUT);  //BLUETOOTH AT-MODE
 }
 
 
@@ -35,57 +47,52 @@ one.toUpperCase();
 if(one.equals("FORWARD")==true){
 forward();
 flag=1;
-bluetooth.println("Forward_10");
+bluetooth.println("Moving Forward");
 }
 else if(one.equals("BACKWARD")==true){
 backward();
 flag=2;  
-bluetooth.println("Backward_10");
+bluetooth.println("Moving Backward");
 
 }
 else if(one.equals("LEFT")==true){
 left();
 flag=3;
-bluetooth.println("Left_10");
+bluetooth.println("Moving Left");
 }
 else if(one.equals("RIGHT")==true){
 right();
 flag=4;
-bluetooth.println("Right_10");
+bluetooth.println("Moving Right");
 }
+
 else{
-bluetooth.println("Brake_10");
+bluetooth.println("Brake");
 brake();  
 flag=0;
 }
  
 } //end of if containing serial.available()
 
-/*
-if((sonar1.ping_cm()>15)||((sonar2.ping_cm>15)&&(flag==2))){
+if((sonar2.ping_cm()>15)&&(sonar1.ping_cm()>15)){
   if(flag==1){
   forward();
-  Serial.println("forward_1");
   }
   else if(flag==2){
   backward();
-  Serial.println("backward_1");
   }
   else if(flag==3){
   left();
-  Serial.println("left_1");
   }
   else if(flag==4){
   right();
-  Serial.println("right_1");
   }
 }
 
-else{
-  Serial.println(sonar.ping_cm()+" BRAKE");
+else if((sonar2.ping_cm()<15)||(sonar1.ping_cm()<15)){
   brake();
 }
-*/
+
 }
 
 
