fb67bae2a6b9c550fcbf0fb07554c8ab6be2cdd2
Vasudev-Sharma-13 <117604604+Vasudev-Sharma-13@users.noreply.github.com>
2023-09-09 10:39:46 -0400

    Setting motors
    Setting motors

--- Wheelchair_arduino/Wheelchair_arduino.ino
+++ Wheelchair_arduino/Wheelchair_arduino.ino
@@ -1,9 +1,16 @@
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(6, OUTPUT);  //LEFT motor
+  pinMode(7, OUTPUT);  //LEFT motor
 
+  pinMode(8, OUTPUT);  //RIGHT motor
+  pinMode(9, OUTPUT);  //RIGHT motor
+
+  pinMode(A0, OUTPUT);   //Right motor
+  pinMode(A1, OUTPUT);   //Left motor
+  analogWrite(A0, 255);  //Right motor speed PWM
+  analogWrite(A1, 255);  //Left motor speed PWM
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
